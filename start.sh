d2392921c615df922296ac5ccbee16aa11a062cf
KUJI <kunjinkao55@outlook.com>
2025-07-24 18:06:04 +0800

    "fix start.sh"

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 cd backend
-#source venv/bin/activate
-#flask init-db            # 只第一次
-flask run &              # 后台
-flask python -m flask run --host=0.0.0.0 --port=5000 &
-#celery -A backend.tasks.celery_app worker --loglevel=info --pool=solo &
+source venv/bin/activate
+python -m flask init-db     # 只第一次运行
+python -m flask run --host=0.0.0.0 --port=5000 &
 cd ../frontend
 npm run dev &
-#node tus-server.js &
